f87935f194029a7ef72468c1001f1367b3060495
Zach Lynch <zachary.lynch1337@gmail.com>
2020-06-18 18:25:25 -0700

    get request using api key to open weather api

---
+++ lib/weather_data.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+# one class will be UserInput, that gets the name of the city the user wants to see
+
+# class 2 will be GetRequester that gets the data of the selected city and parses
+
+#class 3 will be DisplayData that formats and displays data for user to see
+
+class GetRequester
+
+    URL = 'api.openweathermap.org/data/2.5/weather?q=Lasvegas&appid=3e1f0b01ca98c6df658b5b65c8a1654a'
+
+    def initialize(url)
+        @url = url
+
+    end
+
+
+    def get_response_body
+        uri = URI.parse(@url)
+        responce = Net::HTTP.get_response(uri)
+        responce.body
+    end
+
+    def parse_json
+        x = JSON.parse(self.get_response_body)
+        puts x
+
+    end
+
+
+
+end
+x = GetRequester.new('https://api.openweathermap.org/data/2.5/weather?q=lasvegas&appid=f0a3c1953f52f47c3b3c68881c1c7ef8')
+x.parse_json
