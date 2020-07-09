57e039e68e95f1167c90fd4769f89bf381fc7475
Zach Lynch <zachary.lynch1337@gmail.com>
2020-07-08 18:13:50 -0700

    created and modified environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,6 @@
-require 'bundler/setup'
-Bundler.require(:default, :development)
+require 'bundler'
+
+require_relative '../lib/api_call'
+require_relative '../lib/display'
+require_relative '../lib/user_input.rb'
+require_relative '../lib/weather_data.rb'
